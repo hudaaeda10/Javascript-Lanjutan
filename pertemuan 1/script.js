62af83ff23a85d6e802e2b72bcbc626a910c8ac6
Huda Aeda <hudaaeda10@gmail.com>
2021-04-04 13:13:35 +0700

    Pertemuan #1.2 - Object Create

--- pertemuan 1/script.js
+++ pertemuan 1/script.js
@@ -1,6 +1,8 @@
 // Deklarasi Object
 
 // 1. Deklarasi Object Iteration
+// Kelemahannya kurang efisien ketika membuat banyak object
+
 // let mahasiswa1 = {
 //     nama: 'Kana Hanazawa',
 //     energi: 10,
@@ -20,22 +22,40 @@
 
 
 // 2. Deklarasi Function
-// function Mahasiswa(nama, porsi) {
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = porsi;
-//     mahasiswa.makan = function(porsi) {
-//         this.energi += porsi;
-//         console.log(`Halo ${this.nama}, selamat makan!`);
-//     }
-//     mahasiswa.main = function(jam) {
-//         this.energi -= jam;
-//         console.log(`Halo ${this.nama}, selamat bermain!`);
-//     }
-//     return mahasiswa;
-// }
-// let mahasiswa1 = Mahasiswa('Kana Hanazawa', 20);
-// let mahasiswa2 = Mahasiswa('Horimiya', 20);
+//  Mendeklarasikan method-method secara berulang
+// ============== SOLUSI ========================
+//  Menggunakan Object Create untuk memanggil method yang sudah tersimpan di memory
+
+// Buat object baru yang berisi method-method yang akan digunakan di object mahasiswa
+const methodMahasiswa = {
+        makan: function(porsi) {
+            this.energi += porsi;
+            console.log(`Halo ${this.nama}, selamat makan!`);
+        },
+        main: function(jam) {
+            this.energi -= jam;
+            console.log(`Halo ${this.nama}, selamat bermain!`);
+        },
+        tidur: function(jam){
+            this.energi += jam * 2;
+            console.log(`Halo ${this.nama}, selamat tidur!`);
+        }
+}
+
+function Mahasiswa(nama, porsi) {
+    // pasangkan Object.create()  pada variabel yang menyimpan object
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energi = porsi;
+
+    return mahasiswa;
+}
+let mahasiswa1 = Mahasiswa('Kana Hanazawa', 20);
+let mahasiswa2 = Mahasiswa('Horimiya', 20);
+
+
+
+
 
 
 
