365a45f9aedb2652233702053b5d5db730cd3e36
Huda Aeda <hudaaeda10@gmail.com>
2021-04-04 13:43:31 +0700

    Pertemuan 1.3 - Prototype

--- pertemuan 1/script.js
+++ pertemuan 1/script.js
@@ -27,31 +27,86 @@
 //  Menggunakan Object Create untuk memanggil method yang sudah tersimpan di memory
 
 // Buat object baru yang berisi method-method yang akan digunakan di object mahasiswa
-const methodMahasiswa = {
-        makan: function(porsi) {
-            this.energi += porsi;
-            console.log(`Halo ${this.nama}, selamat makan!`);
-        },
-        main: function(jam) {
-            this.energi -= jam;
-            console.log(`Halo ${this.nama}, selamat bermain!`);
-        },
-        tidur: function(jam){
-            this.energi += jam * 2;
-            console.log(`Halo ${this.nama}, selamat tidur!`);
-        }
+// const methodMahasiswa = {
+//         makan: function(porsi) {
+//             this.energi += porsi;
+//             console.log(`Halo ${this.nama}, selamat makan!`);
+//         },
+//         main: function(jam) {
+//             this.energi -= jam;
+//             console.log(`Halo ${this.nama}, selamat bermain!`);
+//         },
+//         tidur: function(jam){
+//             this.energi += jam * 2;
+//             console.log(`Halo ${this.nama}, selamat tidur!`);
+//         }
+// }
+
+// function Mahasiswa(nama, porsi) {
+//     // pasangkan Object.create()  pada variabel yang menyimpan object
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = porsi;
+
+//     return mahasiswa;
+// }
+// let mahasiswa1 = Mahasiswa('Kana Hanazawa', 20);
+// let mahasiswa2 = Mahasiswa('Horimiya', 20);
+
+
+//  Dengan cara diatas memungkinkan kita untuk mengurusi dua object agar tidak perlu membuat object baru maka menggunakan protype
+//  menggunakan constructor function di tiap constructor function terdapat parent function yang dapat dipakai yaitu (PROTOYPE)
+
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+// }
+
+// // menggunakan protoype - NamaFunction.prototype.namaMethod
+// Mahasiswa.prototype.makan = function (porsi) {
+//     this.energi += porsi;
+//     return `Halo ${this.nama}, selamat makan!`;
+// }
+
+// Mahasiswa.prototype.main = function (jam) {
+//     this.energi -= jam;
+//     return `Halo ${this.nama}, selamat main!`;
+// }
+
+// Mahasiswa.prototype.tidur = function(jam) {
+//     this.energi += jam * 2;
+//     return `Halo ${this.nama}, selamat tidur!`;
+// }
+
+
+// let mahasiswa1 = new Mahasiswa('Kana Hanazawa', 20);
+
+//  Menggunakan CLASS dalam mengimplementasikan prototype
+class Mahasiswa {
+    constructor(nama, energi) {
+        this.nama = nama;
+        this.energi = energi;
+    }
+
+    makan (porsi) {
+        this.energi += porsi;
+        return `Halo ${this.nama}, selamat makan!`;
+    }
+
+    main (jam) {
+        this.energi -= jam;
+        return `Halo ${this.nama}, selamat main!`;
+    }
+
+    tidur(jam) {
+        this.energi += jam * 2;
+        return `Halo ${this.nama}, selamat tidur!`;
+    }
 }
 
-function Mahasiswa(nama, porsi) {
-    // pasangkan Object.create()  pada variabel yang menyimpan object
-    let mahasiswa = Object.create(methodMahasiswa);
-    mahasiswa.nama = nama;
-    mahasiswa.energi = porsi;
+let mahasiswa1 = new Mahasiswa('Kana Hanazawa', 10);
 
-    return mahasiswa;
-}
-let mahasiswa1 = Mahasiswa('Kana Hanazawa', 20);
-let mahasiswa2 = Mahasiswa('Horimiya', 20);
+
 
 
 
